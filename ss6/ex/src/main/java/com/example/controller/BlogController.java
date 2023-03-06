1d76c350f95254ada4ade151f5ff690ad7c8df1d
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2023-03-06 09:48:55 +0700

    nop baitap

--- ss6/ex/src/main/java/com/example/controller/BlogController.java
+++ ss6/ex/src/main/java/com/example/controller/BlogController.java
@@ -14,37 +14,33 @@ public class BlogController {
     private IBlogService iBlogService;
 
     @GetMapping("")
-    public String showList(Model model) {
+    public String showList(Model model){
         model.addAttribute("blogList", iBlogService.listAll());
         return "/list";
     }
 
     @GetMapping("/create-form")
-    public String createForm(Model model) {
+    public String createForm(Model model){
         model.addAttribute("blog", new Blog());
         return "/create";
     }
-
     @PostMapping("/create")
-    public String blogCreate(@ModelAttribute Blog blog, Model model) {
+    public String blogCreate(@ModelAttribute Blog blog, Model model){
         iBlogService.create(blog);
         return "redirect:/blog";
     }
-
     @GetMapping("/update-form/{id}")
-    public String updateForm(@PathVariable int id, Model model) {
+    public  String updateForm(@PathVariable int id, Model model){
         model.addAttribute("blogs", iBlogService.findById(id));
         return "/update";
     }
-
     @PostMapping("/update")
-    public String blogUpdate(Blog blog) {
+    public String blogUpdate(Blog blog){
         iBlogService.update(blog);
         return "redirect:/blog";
     }
-
     @GetMapping("/delete/{id}")
-    public String delete(@RequestParam int deleteId) {
+    public String delete(@RequestParam int deleteId){
         iBlogService.delete(deleteId);
         return "redirect:/blog";
     }
